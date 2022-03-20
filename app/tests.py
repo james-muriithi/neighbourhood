65f7f4cffa28037022e930fb937dba0c5de8cde6
james-muriithi <muriithijames556@gmail.com>
2022-03-20 21:41:33 +0300

    added neighbourhood tests

--- app/tests.py
+++ app/tests.py
@@ -58,3 +58,46 @@ class PostTestClass(TestCase):  # Project class test
         self.post.delete_post()
         post = Post.objects.filter(id=self.post.id).exists()
         self.assertFalse(post)
+
+
+class NeighbourhoodTestClass(TestCase):  # Neighbourhood class test
+    def setUp(self):
+        location = Location(
+            name='Nairobi'
+        )
+
+        location.save_location()
+
+        # create a user
+        user = User.objects.create(
+            username="john", full_name="john doe", email="m@.com",
+            location_id=location.id
+        )
+
+        user.save()
+
+        self.hood = NeighbourHood(
+            name="example neighbourhood",
+            location=location,
+            admin=user
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood, NeighbourHood))
+
+    def test_save_method(self):
+        self.hood.create_neigborhood()
+        hood = NeighbourHood.find_neigborhood(self.hood.id)
+        self.assertIsNotNone(hood)
+
+    def test_update_method(self):
+        self.hood.name = 'new name'
+        self.hood.save()
+        hood = NeighbourHood.find_neigborhood(self.host.id)
+        self.assertEqual(self.hood.name, hood.name)
+
+    def test_delete_method(self):
+        self.hood.create_neigborhood()
+        self.hood.delete_neighbourhood()
+        hood = NeighbourHood.objects.filter(id=self.hood.id).exists()
+        self.assertFalse(hood)
